fcc9937fc412fcb2635306206f10b4e01d78a353
maynkj <mayn.kjaer@gmail.com>
2018-01-02 21:45:15 +0100

    Create new todo.

--- src/controllers/todos_controller.js
+++ src/controllers/todos_controller.js
@@ -4,10 +4,36 @@ export default class extends Controller {
   create(e) {
     e.preventDefault();
 
-    console.log(this.title);
+    const title = this.title;
+
+    this.todos = [
+      ...this.todos,
+      title
+    ];
+
+    const el = document.createElement("li");
+    el.textContent = title;
+
+    this.todosList.append(el);
   }
 
   get title() {
     return this.targets.find("title").value;
+  }
+
+  get todosList() {
+    return this.targets.find("todosList");
+  }
+
+  get todos() {
+    if(this.data.has("todos")) {
+      return JSON.parse(this.data.get("todos"));
+    } else {
+      return [];
+    }
+  }
+
+  set todos(arr) {
+    this.data.set("todos", JSON.stringify(arr));
   }
 }
